97f41282d2bb12ba21ea2452e55c713e354e3385
Dcab117 <danielcabri@gmail.com>
2016-03-17 13:37:57 -0430

    Nomital

---
+++ Nomital/Nomital/platforms/android/assets/www/js/Register.js
@@ -0,0 +1,93 @@
+﻿angular.module('ionicApp', ['ionic'])
+
+.config(function ($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('signin', {
+          url: '/sign-in',
+          templateUrl: 'templates/sign-in.html',
+          controller: 'SignInCtrl'
+      })
+      .state('sign', {
+          url: '/sign.html',
+          templateUrl: 'templates/sign.html',
+          controller: 'RegisterCtrl'
+      })
+      .state('tabs', {
+          url: '/tab',
+          abstract: true,
+          templateUrl: 'templates/tabs.html'
+      })
+      .state('tabs.home', {
+          url: '/home',
+          views: {
+              'home-tab': {
+                  templateUrl: 'templates/home.html',
+                  controller: 'HomeTabCtrl'
+              }
+          }
+      })
+      .state('tabs.facts', {
+          url: '/facts',
+          views: {
+              'home-tab': {
+                  templateUrl: 'templates/facts.html'
+              }
+          }
+      })
+      .state('tabs.facts2', {
+          url: '/facts2',
+          views: {
+              'home-tab': {
+                  templateUrl: 'templates/facts2.html'
+              }
+          }
+      })
+      .state('tabs.about', {
+          url: '/about',
+          views: {
+              'about-tab': {
+                  templateUrl: 'templates/about.html'
+              }
+          }
+      })
+      .state('tabs.navstack', {
+          url: '/navstack',
+          views: {
+              'about-tab': {
+                  templateUrl: 'templates/nav-stack.html'
+              }
+          }
+      })
+      .state('tabs.contact', {
+          url: '/contact',
+          views: {
+              'contact-tab': {
+                  templateUrl: 'templates/contact.html'
+              }
+          }
+      });
+
+
+    $urlRouterProvider.otherwise('/sign-in');
+
+})
+
+.controller('SignInCtrl', function ($scope, $state) {
+
+    $scope.signIn = function (user) {
+        console.log('Sign-In', user);
+        $state.go('sign');
+    };
+
+})
+
+
+.controller('RegisterCtrl', function ($scope, $state) {
+
+    $scope.register = function (user) {
+        console.log('Register', "Funciona");
+        
+    };
+
+});
---
+++ Nomital/Nomital/www/js/Register.js
@@ -0,0 +1,49 @@
+﻿angular.module('ionicApp', ['ionic'])
+
+.config(function ($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('signin', {
+          url: '/sign-in',
+          templateUrl: 'templates/sign-in.html',
+          controller: 'SignInCtrl'
+      })
+      .state('sign', {
+          url: '/sign.html',
+          templateUrl: 'templates/sign.html',
+          controller: 'RegisterCtrl'
+      })
+      .state('MenuPrincipalCobranza', {
+          url: '/MenuPrincipalCobranza.html',
+          templateUrl: 'templates/MenuPrincipalCobranza.html',
+          controller: 'MenuPrincipalCobranzaCtrl'
+      });
+
+
+    $urlRouterProvider.otherwise('/sign-in');
+
+})
+
+.controller('SignInCtrl', function ($scope, $state) {
+
+    $scope.signIn = function (user) {
+        console.log('Sign-In', user);
+        $state.go('sign');
+    };
+
+})
+
+
+.controller('RegisterCtrl', function ($scope, $state) {
+
+    $scope.register = function (user) {
+        
+
+        
+        window.location.href = "MenuPrincipal.html";
+    };
+
+})
+
+
+
